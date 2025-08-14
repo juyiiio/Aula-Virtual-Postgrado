b0cc0d3469ee26a00a01155f8de439883a615fd0
abel-ov1 <abeloliva2022@gmail.com>
2025-08-14 17:48:42 -0500

    Update Button.jsx

--- aula-virtual-frontend/src/components/common/Button/Button.jsx
+++ aula-virtual-frontend/src/components/common/Button/Button.jsx
@@ -1,51 +1,42 @@
 import React from 'react';
-import classNames from 'classnames';
 import styles from './Button.module.css';
 
 const Button = ({
   children,
-  type = 'button',
   variant = 'primary',
   size = 'medium',
   disabled = false,
   loading = false,
-  icon,
-  iconPosition = 'left',
-  fullWidth = false,
   onClick,
-  className,
+  type = 'button',
+  className = '',
   ...props
 }) => {
-  const buttonClasses = classNames(
+  const handleClick = (e) => {
+    if (!disabled && !loading && onClick) {
+      onClick(e);
+    }
+  };
+
+  const buttonClass = [
     styles.button,
     styles[variant],
     styles[size],
-    {
-      [styles.disabled]: disabled,
-      [styles.loading]: loading,
-      [styles.fullWidth]: fullWidth,
-      [styles.iconRight]: iconPosition === 'right'
-    },
+    disabled && styles.disabled,
+    loading && styles.loading,
     className
-  );
-
-  const handleClick = (e) => {
-    if (disabled || loading) return;
-    onClick && onClick(e);
-  };
+  ].filter(Boolean).join(' ');
 
   return (
     <button
       type={type}
-      className={buttonClasses}
-      disabled={disabled || loading}
+      className={buttonClass}
       onClick={handleClick}
+      disabled={disabled || loading}
       {...props}
     >
-      {loading && <div className={styles.spinner} />}
-      {icon && iconPosition === 'left' && <span className={styles.icon}>{icon}</span>}
-      <span className={styles.content}>{children}</span>
-      {icon && iconPosition === 'right' && <span className={styles.icon}>{icon}</span>}
+      {loading && <span className={styles.spinner}></span>}
+      <span className={loading ? styles.hiddenText : ''}>{children}</span>
     </button>
   );
 };
