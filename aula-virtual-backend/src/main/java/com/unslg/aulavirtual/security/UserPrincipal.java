34c2467d4c039e18013b02fa62caa60ee0f5a8d6
abel-ov1 <abeloliva2022@gmail.com>
2025-08-14 19:50:38 -0500

    Update UserPrincipal.java

--- aula-virtual-backend/src/main/java/com/unslg/aulavirtual/security/UserPrincipal.java
+++ aula-virtual-backend/src/main/java/com/unslg/aulavirtual/security/UserPrincipal.java
@@ -1,1 +1,69 @@
+package com.unslg.aulavirtual.security;
+
+import com.unslg.aulavirtual.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class UserPrincipal implements UserDetails {
+    private Long id;
+    private String userCode;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserPrincipal create(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new UserPrincipal(
+                user.getId(),
+                user.getUserCode(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
 
