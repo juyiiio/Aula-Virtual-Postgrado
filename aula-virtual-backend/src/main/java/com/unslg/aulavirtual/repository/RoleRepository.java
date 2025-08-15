230e6f4bd0fb2ddf85b483c85fd153e8689949b3
abel-ov1 <abeloliva2022@gmail.com>
2025-08-14 19:44:44 -0500

    Update RoleRepository.java

---
+++ aula-virtual-backend/src/main/java/com/unslg/aulavirtual/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.unslg.aulavirtual.repository;
+
+import com.unslg.aulavirtual.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    
+    Optional<Role> findByName(Role.RoleName name);
+    
+    Boolean existsByName(Role.RoleName name);
+}
